8ba708bc619ec48ceb7ffc4fb335002026b4c9ac
WanilkaGIT <mahlunivan27@gmail.com>
2024-09-21 17:25:25 +0300

    Це все що вийшло

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=400)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=100000 ,decimal_places=2)
+
+class Review(models.Model):
+    # product = models.ForeignKey()
+    author = models.CharField(max_length=400)
+    text_review = models.TextField()
+    rating = models.IntegerField()
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product, Review
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "render_string": "First page"
+    }
+    return render(request, template_name="myapp/index.html", context=context)
